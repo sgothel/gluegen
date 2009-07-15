c9797e46befa72195fe65a64f65260d88ea575a4
sg215889 <sg215889@jordan.goethel.localnet>
2009-07-15 07:14:45 -0700

    Tests [PointerBuffer,StructAccessor]: Add diagnostics

--- src/java/com/sun/gluegen/test/TestStructAccessorEndian.java
+++ src/java/com/sun/gluegen/test/TestStructAccessorEndian.java
@@ -7,7 +7,11 @@ import java.nio.*;
 public class TestStructAccessorEndian {
     public static void main (String args[]) {
         boolean ok = true;
-        System.out.println("CPU is : "+ (BufferFactory.isLittleEndian()?"little":"big") + " endian");
+        String os = System.getProperty("os.name");
+        String cpu = System.getProperty("os.arch");
+        System.out.println("OS: <"+os+"> CPU: <"+cpu+">");
+        System.out.println("CPU is: "+ (CPU.is32Bit()?"32":"64") + " bit");
+        System.out.println("Buffer is in: "+ (BufferFactory.isLittleEndian()?"little":"big") + " endian");
         ByteBuffer tst = BufferFactory.newDirectByteBuffer(BufferFactory.SIZEOF_LONG * 3);
         StructAccessor acc = new StructAccessor(tst);
         acc.setLongAt(0, 0x0123456789ABCDEFL);
