79e0951cfdb5fb2371df23edeaf7f8957b0ee3f5
Michael Bien <mbien@fh-landshut.de>
2009-10-18 19:17:30 +0200

    added first JUnit test: GlueGenTest.java. (dependencies: junit-4.5.jar, antlr-2.7.7.jar in <gluegenroot>/lib) Test tests the basic GlueGen lifecycle: generate->compile java->compile native->load lib->call methods integrated test compile,run and debug targets into NetBeans IDE.

---
+++ test/junit/com/sun/gluegen/test.h
@@ -0,0 +1,10 @@
+
+typedef unsigned long foo;
+
+int arrayTest(long context, foo * array );
+
+int bufferTest(void * object);
+
+int mixedTest(long context, void * object, foo * array );
+
+int doubleTest(long context, void * object1, foo * array1, void * object2, foo * array2 );
