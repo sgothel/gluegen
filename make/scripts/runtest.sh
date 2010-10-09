52dcea28906b3e61ef44595399e525dd5169c014
Sven Gothel <sgothel@jausoft.com>
2010-10-09 04:35:53 +0200

    fix runtest.sh scripts

--- make/scripts/runtest.sh
+++ make/scripts/runtest.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 
 builddir=$1
 shift
@@ -8,9 +8,14 @@ if [ -z "$builddir" ] ; then
     exit 1
 fi
 
-echo com.jogamp.gluegen.test.TestPointerBufferEndian
-java -Djava.library.path=$builddir/obj -classpath $builddir/classes  com.jogamp.gluegen.test.TestPointerBufferEndian
-echo
-echo com.jogamp.gluegen.test.TestStructAccessorEndian
-java -Djava.library.path=$builddir/obj -classpath $builddir/classes  com.jogamp.gluegen.test.TestStructAccessorEndian
-echo
+function onetest() {
+    clazz=$1
+    shift
+    echo $clazz
+    java -Djava.library.path=$builddir/obj:$builddir/test/build/natives -classpath lib/junit.jar:$builddir/classes:$builddir/test/build/classes $clazz
+    echo
+}
+
+onetest com.jogamp.common.util.TestRecursiveToolkitLock
+#onetest com.jogamp.gluegen.test.TestPointerBufferEndian
+#onetest com.jogamp.gluegen.test.TestStructAccessorEndian
