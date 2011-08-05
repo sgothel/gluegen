e70e44922955dfcf26b222ed36754dea00253bde
Sven Gothel <sgothel@jausoft.com>
2011-08-05 13:40:13 +0200

    crosstest: exclude .git as well

--- make/scripts/crosstest-java-android-armv7-rel.sh
+++ make/scripts/crosstest-java-android-armv7-rel.sh
@@ -33,7 +33,7 @@ LOGFILE=`basename $0 .sh`.log
 #  -Djava.library.path=/system/lib:$TARGET_ROOT/gluegen/make/$BUILD_DIR/obj:$BUILD_DIR/test/build/natives \
 
 RSYNC_EXCLUDES="--exclude 'build-x86*/' --exclude 'build-linux*/' --exclude 'build-win*/' --exclude 'build-mac*/' \
-                --exclude 'classes/' --exclude 'src/' \
+                --exclude 'classes/' --exclude 'src/' --exclude '.git/' \
                 --delete-excluded"
 
 echo "#! /system/bin/sh" > $BUILD_DIR/targetcommand.sh
