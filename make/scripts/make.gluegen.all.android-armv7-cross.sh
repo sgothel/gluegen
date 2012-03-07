85d3068b842af1aa50295a652e037ecb2510d765
Sven Gothel <sgothel@jausoft.com>
2012-03-07 04:29:31 +0100

    gluegen-cpptasks-android-armv7: Add 'gluegen.cpptasks.detect.os.custom' target which makes it self-contained
    build script and jenkins-node startup-script solely use env-vars.
    
    Remove env. TARGET_ARCH, which was same as TARGET_TRIPPLE.

--- make/scripts/make.gluegen.all.android-armv7-cross.sh
+++ make/scripts/make.gluegen.all.android-armv7-cross.sh
@@ -8,14 +8,13 @@ export HOST_IP=10.1.0.122
 export HOST_RSYNC_ROOT=PROJECTS/JOGL
 
 export TARGET_UID=jogamp
+#export TARGET_IP=panda02
 #export TARGET_IP=jautab03
 export TARGET_IP=jauphone04
 export TARGET_ADB_PORT=5555
 # needs executable bit (probably su)
 export TARGET_ROOT=/data/projects
 export TARGET_ANT_HOME=/usr/share/ant
-
-export ANDROID_VERSION=9
 
 echo ANDROID_SDK_HOME $ANDROID_SDK_HOME
 echo NDK_ROOT $NDK_ROOT
@@ -66,30 +65,28 @@ elif [ ! -e $ANDROID_SDK_HOME ] ; then
 fi
 export ANDROID_SDK_HOME
 
+export ANDROID_VERSION=9
+export SOURCE_LEVEL=1.6
+export TARGET_LEVEL=1.6
+export TARGET_RT_JAR=/opt-share/jre1.6.0_30/lib/rt.jar
+
 export GCC_VERSION=4.4.3
 HOST_ARCH=linux-x86
-export TARGET_ARCH=arm-linux-androideabi
 export TARGET_TRIPLE=arm-linux-androideabi
 
-export NDK_TOOLCHAIN_ROOT=$NDK_ROOT/toolchains/${TARGET_ARCH}-${GCC_VERSION}/prebuilt/${HOST_ARCH}
+export NDK_TOOLCHAIN_ROOT=$NDK_ROOT/toolchains/${TARGET_TRIPLE}-${GCC_VERSION}/prebuilt/${HOST_ARCH}
 export TARGET_PLATFORM_ROOT=${NDK_ROOT}/platforms/android-${ANDROID_VERSION}/arch-arm
 
 # Need to add toolchain bins to the PATH. 
 export PATH="$NDK_TOOLCHAIN_ROOT/$TARGET_TRIPLE/bin:$ANDROID_SDK_HOME/platform-tools:$PATH"
 
+export GLUEGEN_CPPTASKS_FILE="lib/gluegen-cpptasks-android-armv7.xml"
+
+#export JUNIT_DISABLED="true"
+#export JUNIT_RUN_ARG0="-Dnewt.test.Screen.disableScreenMode"
+
 which gcc 2>&1 | tee make.gluegen.all.android-armv7-cross.log
 
 ant \
-    -Dtarget.sourcelevel=1.6 \
-    -Dtarget.targetlevel=1.6 \
-    -Dtarget.rt.jar=/opt-share/jre1.6.0_30/lib/rt.jar \
-    -Dgluegen-cpptasks.file=`pwd`/lib/gluegen-cpptasks-android-armv7.xml \
     -Drootrel.build=build-android-armv7 \
-    -Dgluegen.cpptasks.detected.os=true \
-    -DisUnix=true \
-    -DisAndroid=true \
-    -DisAndroidARMv7=true \
-    -DjvmDataModel.arg="-Djnlp.no.jvm.data.model.set=true" \
-    -DisCrosscompilation=true \
-    -Dandroid.abi=armeabi-v7a \
     $* 2>&1 | tee -a make.gluegen.all.android-armv7-cross.log
