4b70739956713423137a0918e834f8ed1f42fd44
Sven Gothel <sgothel@jausoft.com>
2012-05-04 03:54:17 +0200

    Android ClassLoaderUtil: Cleanup hack ..

--- src/java/jogamp/android/launcher/ClassLoaderUtil.java
+++ src/java/jogamp/android/launcher/ClassLoaderUtil.java
@@ -43,7 +43,6 @@ public class ClassLoaderUtil {
    
    // FIXME: Need to generalize this .. (Note: native lib resources must be cached!)
    private static final String[] packagesJogAmp = { "com.jogamp.common", "javax.media.opengl" };   
-   // JAU private static final String[] packagesJogAmp = { "javax.media.opengl" };
    private static ClassLoader jogAmpClassLoader = null;
    
    // location where optimized dex files will be written
@@ -105,7 +104,6 @@ public class ClassLoaderUtil {
        
        StringBuilder apks = new StringBuilder();
        StringBuilder libs = new StringBuilder();
-       // JAU libs.append("/data/data/com.jogamp.common/").append(ELEM_SEP);
        int apkCount = 0;
        String lastUserPackageName = null; // the very last one reflects the Activity
        
