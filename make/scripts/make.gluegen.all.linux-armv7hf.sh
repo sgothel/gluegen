09050d80ed163f1468b8048cfd037772de316eee
Xerxes Rånby <xerxes@zafena.se>
2012-08-07 01:10:17 +0000

    Use armv6 optimization for armhf to stay compatible with the new Raspberry Pi Raspbian distribution.

---
+++ make/scripts/make.gluegen.all.linux-armv7hf.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+#    -Dc.compiler.debug=true 
+#    -Dgluegen.cpptasks.detected.os=true \
+#    -DisUnix=true \
+#    -DisLinux=true \
+#    -DisLinuxX86=true \
+#    -DisX11=true \
+
+# arm-linux-gnueabihf == armhf triplet
+export TARGET_PLATFORM_LIBS=/usr/lib/arm-linux-gnueabihf
+export TARGET_JAVA_LIBS=/usr/lib/jvm/java-6-openjdk-armhf/jre/lib/arm
+
+export GLUEGEN_CPPTASKS_FILE="lib/gluegen-cpptasks-linux-armv6hf.xml"
+
+ant \
+    -Drootrel.build=build-linux-armv7hf \
+    $* 2>&1 | tee make.gluegen.all.linux-armv7hf.log
