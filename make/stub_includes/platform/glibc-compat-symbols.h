eb8ba86c01d502a04dc50150bda5f5b71dc03ca5
Sven Gothel <sgothel@jausoft.com>
2013-04-21 09:01:01 +0200

    glibc-compat-symbols.h: Add __arm__ -> GLIBC_2.4 ; default is now GLIBC_2.2.5 // Still minimum required is GLIBC_2.4!

--- make/stub_includes/platform/glibc-compat-symbols.h
+++ make/stub_includes/platform/glibc-compat-symbols.h
@@ -2,12 +2,22 @@
 #define __GLIBC_COMPAT_SYMBOLS_H__ 1
  
 /**
- * add other architecures below
+ *
+ * Note: JogAmp's minimum GLIBC is 2.4 due to '__stack_chk_fail' (stack overflow checking)
+ *   
+ *   GLIBC 2.4 - March 2006  - Standard for LSB 4.0, Used in SLES 10
+ *   
+ * We could add compile/link option '-fno-stack-protector', however stack protection seems reasonable
+ * and a pre 2006 distribution a bit too 'far fetched' for our multimedia bindings anyway.
+ *
+ * Check build-in macro definitions via 'gcc -dM -E - < /dev/null'
  */
-#ifdef __amd64__
+#if defined(__arm__)
+   #define GLIBC_COMPAT_SYMBOL(FFF) __asm__(".symver " #FFF "," #FFF "@GLIBC_2.4");
+#elif defined(__amd64__)
    #define GLIBC_COMPAT_SYMBOL(FFF) __asm__(".symver " #FFF "," #FFF "@GLIBC_2.2.5");
 #else
-   #define GLIBC_COMPAT_SYMBOL(FFF) __asm__(".symver " #FFF "," #FFF "@GLIBC_2.0");
+   #define GLIBC_COMPAT_SYMBOL(FFF) __asm__(".symver " #FFF "," #FFF "@GLIBC_2.2.5");
 #endif /*__amd64__*/
 
 GLIBC_COMPAT_SYMBOL(memcpy)
