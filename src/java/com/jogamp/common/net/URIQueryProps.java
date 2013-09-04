c47bac817d39fda74f757da79f781190fe3ca581
Harvey Harrison <harvey.harrison@gmail.com>
2013-09-04 15:52:17 -0700

    gluegen: remove trailing whitespace from URIQueryProps
    Signed-off-by: Harvey Harrison <harvey.harrison@gmail.com>

--- src/java/com/jogamp/common/net/URIQueryProps.java
+++ src/java/com/jogamp/common/net/URIQueryProps.java
@@ -3,14 +3,14 @@
  *
  * Redistribution and use in source and binary forms, with or without modification, are
  * permitted provided that the following conditions are met:
- * 
+ *
  *    1. Redistributions of source code must retain the above copyright notice, this list of
  *       conditions and the following disclaimer.
- * 
+ *
  *    2. Redistributions in binary form must reproduce the above copyright notice, this list
  *       of conditions and the following disclaimer in the documentation and/or other materials
  *       provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY JogAmp Community ``AS IS'' AND ANY EXPRESS OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL JogAmp Community OR
@@ -20,7 +20,7 @@
  * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * The views and conclusions contained in the software and documentation are those of the
  * authors and should not be interpreted as representing official policies, either expressed
  * or implied, of JogAmp Community.
@@ -49,16 +49,16 @@ public class URIQueryProps {
    private static final char ASSIG = '=';
    private static final String EMPTY = "";
    private final String query_separator;
-   
+
    private final HashMap<String, String> properties = new HashMap<String, String>();
-   
+
    private URIQueryProps(char querySeparator) {
        query_separator = String.valueOf(querySeparator);
    }
-   
+
    public final Map<String, String> getProperties() { return properties; }
    public final char getQuerySeparator() { return query_separator.charAt(0); }
-   
+
    public final String appendQuery(String baseQuery) throws URISyntaxException {
        boolean needsSep = false;
        final StringBuilder sb = new StringBuilder();
@@ -86,19 +86,19 @@ public class URIQueryProps {
        }
        return sb.toString();
    }
-   
+
    public final URI appendQuery(URI base) throws URISyntaxException {
        return new URI(base.getScheme(),
                       base.getRawUserInfo(), base.getHost(), base.getPort(),
                       base.getRawPath(), appendQuery(base.getRawQuery()), base.getRawFragment());
    }
-   
+
    /**
-    * 
+    *
     * @param uri
     * @param querySeparator should be either <i>;</i> or <i>&</i>, <i>;</i> is encouraged due to troubles of escaping <i>&</i>.
     * @return
-    * @throws IllegalArgumentException if <code>querySeparator</code> is illegal, i.e. neither <i>;</i> nor <i>&</i> 
+    * @throws IllegalArgumentException if <code>querySeparator</code> is illegal, i.e. neither <i>;</i> nor <i>&</i>
     */
    public static final URIQueryProps create(URI uri, char querySeparator) throws IllegalArgumentException {
        if( ';' != querySeparator && '&' != querySeparator ) {
@@ -113,7 +113,7 @@ public class URIQueryProps {
            q_e = q.indexOf(querySeparator, q_b);
            if(0 == q_e) {
                // single separator
-               continue; 
+               continue;
            }
            if(0 > q_e) {
                // end
