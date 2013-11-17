e0877095c24f08fe1e5fc0391edf26392d1b6206
Sven Gothel <sgothel@jausoft.com>
2013-11-17 05:25:30 +0100

    Add generic 'check-glibc.sh' script / rename old script

--- make/scripts/check-glibc.sh
+++ make/scripts/check-glibc.sh
@@ -1,22 +1,26 @@
 #! /bin/bash
 
+BDIR=$1
+shift
+
 SDIR=`dirname $0`
-XDIR=$SDIR/../../make/lib/toolchain
 RDIR=$SDIR/../../..
 
 function check_glibc() {
-  OBJDUMP=$1/bin/objdump
 
   echo "------------------------------------------------------------"
-  echo $2 via $OBJDUMP
+  echo $1 via objdump
   echo
-  $OBJDUMP -T $2 | grep GLIBC | awk ' { print $5 " " $6 } ' | sort
+  objdump -T $1 | grep GLIBC | awk ' { print $5 " " $6 } ' | sort
   echo "------------------------------------------------------------"
 }
 
-check_glibc $XDIR/armsf-linux-gnueabi $SDIR/../ $RDIR/gluegen/build-linux-armv6/obj/libgluegen-rt.so 
-check_glibc $XDIR/armhf-linux-gnueabi $RDIR/gluegen/build-linux-armv6hf/obj/libgluegen-rt.so 
-check_glibc $XDIR/armsf-linux-gnueabi $RDIR/joal/build-linux-armv6/obj/libopenal.so 
-check_glibc $XDIR/armsf-linux-gnueabi $RDIR/joal/build-linux-armv6/obj/libjoal.so 
-check_glibc $XDIR/armhf-linux-gnueabi $RDIR/joal/build-linux-armv6hf/obj/libopenal.so 
-check_glibc $XDIR/armhf-linux-gnueabi $RDIR/joal/build-linux-armv6hf/obj/libjoal.so 
+check_glibc $RDIR/gluegen/$BDIR/obj/libgluegen-rt.so 
+check_glibc $RDIR/joal/$BDIR/obj/libopenal.so 
+check_glibc $RDIR/joal/$BDIR/obj/libjoal.so 
+check_glibc $RDIR/jogl/$BDIR/nativewindow/obj/libnativewindow_awt.so
+check_glibc $RDIR/jogl/$BDIR/nativewindow/obj/libnativewindow_x11.so
+check_glibc $RDIR/jogl/$BDIR/jogl/obj/libjogl_desktop.so
+check_glibc $RDIR/jogl/$BDIR/jogl/obj/libjogl_mobile.so
+check_glibc $RDIR/jogl/$BDIR/jogl/obj/libjogl_cg.so
+check_glibc $RDIR/jogl/$BDIR/newt/obj/libnewt.so
