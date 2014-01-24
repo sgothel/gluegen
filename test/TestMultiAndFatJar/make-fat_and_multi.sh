2868816971bb80eb226e6edffc527d909ced755b
Sven Gothel <sgothel@jausoft.com>
2014-01-24 02:44:26 +0100

    Refine test/TestMultiAndFatJar: Demoing test class outside of fat and multi jar
    Use 'output' folder
    
    - Copy jogl-test.jar and junit.jar to output, to be used in executing (run-*.sh)
    
    Works here ..

--- test/TestMultiAndFatJar/make-fat_and_multi.sh
+++ test/TestMultiAndFatJar/make-fat_and_multi.sh
@@ -5,11 +5,12 @@ JOGL=$THISDIR/../../../jogl
 bdir=build-x86_64
 
 rm -rf temp
+rm -rf output
+mkdir output
 
 #
 # make fat
 #
-rm -f jogl-fat.jar
 mkdir temp
 cd temp
 unzip -o $GLUEGEN/$bdir/gluegen-rt.jar 
@@ -19,27 +20,33 @@ unzip -o $JOGL/$bdir/jar/jogl-all-natives-linux-amd64.jar
 mkdir -p natives/linux-amd64
 mv lib*.so natives/linux-amd64/
 rm -rf META-INF
-jar cf ../jogl-fat.jar *
+jar cf ../output/jogl-fat.jar *
 cd ..
 rm -rf temp
 
 #
 # make multi
 #
-rm -f jogl-multi.jar jogl-multi-natives-linux-amd64.jar
 mkdir temp
 cd temp
 unzip -o $GLUEGEN/$bdir/gluegen-rt.jar 
 unzip -o $JOGL/$bdir/jar/jogl-all.jar
 rm -rf META-INF
-jar cf ../jogl-multi.jar *
+jar cf ../output/jogl-multi.jar *
 rm -rf *
 unzip -o $GLUEGEN/$bdir/gluegen-rt-natives-linux-amd64.jar 
 unzip -o $JOGL/$bdir/jar/jogl-all-natives-linux-amd64.jar
 mkdir -p natives/linux-amd64
 mv lib*.so natives/linux-amd64/
 rm -rf META-INF
-jar cf ../jogl-multi-natives-linux-amd64.jar *
+jar cf ../output/jogl-multi-natives-linux-amd64.jar *
+
 cd ..
 rm -rf temp
+
+#
+# Just to run some tests
+#
+cp -a $JOGL/$bdir/jar/jogl-test.jar output/
+cp -a $GLUEGEN/make/lib/junit.jar output/
 
--- test/TestMultiAndFatJar/run-multi.sh
+++ test/TestMultiAndFatJar/run-multi.sh
@@ -1,9 +1,14 @@
+#! /bin/bash
 
-D_ARGS="-Djogamp.debug.JNILibLoader -Djogamp.debug.TempFileCache -Djogamp.debug.JarUtil -Djogamp.debug.TempJarCache -Djogamp.debug.IOUtil"
+#D_ARGS="-Djogamp.debug.JNILibLoader -Djogamp.debug.TempFileCache -Djogamp.debug.JarUtil -Djogamp.debug.TempJarCache -Djogamp.debug.IOUtil"
 #D_ARGS="-Djogamp.debug.ProcAddressHelper -Djogamp.debug.NativeLibrary -Djogamp.debug.NativeLibrary.Lookup"
 #D_ARGS="-Djogamp.debug.JNILibLoader -Djogamp.debug.TempFileCache -Djogamp.debug.JarUtil -Djogamp.debug.TempJarCache -Djogamp.debug.NativeLibrary -Djogamp.debug.IOUtil"
 
 #T_CLASS="com.jogamp.opengl.JoglVersion"
-T_CLASS="com.jogamp.newt.opengl.GLWindow"
+#T_CLASS="com.jogamp.newt.opengl.GLWindow"
+T_CLASS="com.jogamp.opengl.test.junit.jogl.demos.es2.newt.TestGearsES2NEWT -time 2000"
 
-java -cp jogl-multi.jar $D_ARGS $T_CLASS 2>&1 | tee run-multi.log
+#
+# Just to run some tests: jogl-test.jar and junit.jar
+#
+java -cp output/jogl-multi.jar:output/jogl-test.jar:output/junit.jar $D_ARGS $T_CLASS 2>&1 | tee run-multi.log
