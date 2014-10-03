9b4656a2b676bcad43e67e5ee47f4ed00399a83a
Sven Gothel <sgothel@jausoft.com>
2014-10-03 03:13:42 +0200

    Make Scripts OSX: Remove -java7 script, add -java6 script (default uses java8)

--- make/scripts/make.gluegen.all.macosx-java6.sh
+++ make/scripts/make.gluegen.all.macosx-java6.sh
@@ -10,13 +10,20 @@ fi
 #    -Dtarget.targetlevel=1.6 \
 #    -Dtarget.rt.jar=/opt-share/jre1.6.0_30/lib/rt.jar \
 
-JAVA_HOME=`/usr/libexec/java_home -version 1.7`
+# Force OSX SDK 10.6, if desired
+# export SDKROOT=macosx10.6
+
+JAVA_HOME=`/usr/libexec/java_home -version 1.6`
 PATH=$JAVA_HOME/bin:$PATH
 export JAVA_HOME PATH
+
+export SOURCE_LEVEL=1.6
+export TARGET_LEVEL=1.6
+export TARGET_RT_JAR=/opt-share/jre1.6.0_30/lib/rt.jar
 
 #export JOGAMP_JAR_CODEBASE="Codebase: *.jogamp.org"
 export JOGAMP_JAR_CODEBASE="Codebase: *.goethel.localnet"
 
 ant \
-    -Drootrel.build=build-macosx-java7 \
-    $* 2>&1 | tee make.gluegen.all.macosx-java7.log
+    -Drootrel.build=build-macosx \
+    $* 2>&1 | tee make.gluegen.all.macosx.log
--- make/scripts/make.gluegen.all.macosx.sh
+++ make/scripts/make.gluegen.all.macosx.sh
@@ -10,7 +10,10 @@ fi
 #    -Dtarget.targetlevel=1.6 \
 #    -Dtarget.rt.jar=/opt-share/jre1.6.0_30/lib/rt.jar \
 
-JAVA_HOME=`/usr/libexec/java_home -version 1.7`
+# Force OSX SDK 10.6, if desired
+# export SDKROOT=macosx10.6
+
+JAVA_HOME=`/usr/libexec/java_home -version 1.8`
 PATH=$JAVA_HOME/bin:$PATH
 export JAVA_HOME PATH
 
