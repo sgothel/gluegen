181dc8ae227cd5cbecf60ab8d973bb8eef7c45f0
Sven Gothel <sgothel@jausoft.com>
2014-12-13 06:38:59 +0100

    Bug 1108 - Fix difficulties to determine executable access permission on Windows via BATCH file execution
    Batch file execution test via direct call, i.e.'a.bat',
    does not work on some Windows machine, not reproducible here!
    
    A workaround would be to call the batch file explicit via 'CMD /c a.bat':
      - works when using 'Software Restriction Policies' (Bug 1015 Comment 2)
      - does _not_ work when denying ACL "Traverse Folder / Execute File" permission (Bug 1015 Comment 3)
    
    Due to this bug, we need to use a native execution:
    
    - Performing executable test w/ native exe file instead of batch file
      on Windows x86 32bit and 64bit,
    
    - using [1] TinyPE XP-W8 Compatible - x86 32bit and 64bit - 268 bytes
      - Tested on: WinXP-32bit, Win7-64bit and Win8-64bit
      - Author/License: Ange Albertini, BSD Licence, 2010-2013
    
    - On all other Windows machines (ARM, ..),
      we still use direct execution of 'a.bat'
      but may add native exe files for missing platforms.
    
    +++
    
    This patch injects said binaries within the java jar file
    and copies it into the 'to be tested' temp folder for execution.
    
    [1] TinyPE XP-W8 Compatible - x86 32bit and 64bit - 268 bytes
    is included within 'src/native/tinype-corkami',
    build manually and imported as 'src/java/com/jogamp/common/util/bin/exe-windows-i586-268b.bin'.
    
    +++

---
+++ src/native/tinype-corkami/make.bat
@@ -0,0 +1,13 @@
+REM
+REM Using yasm 1.2.0 win64 or win32
+REM    http://yasm.tortall.net/
+REM
+REM TinyPE XP-W8 Compatible - x86 32bit and 64bit - 268 bytes
+REM    https://code.google.com/p/corkami/wiki/PE
+REM    https://code.google.com/p/corkami/source/browse/trunk/src/PE/tiny.asm
+REM
+REM See also:
+REM    http://www.phreedom.org/research/tinype/
+REM
+c:\yasm\yasm.exe -f bin -o tiny-simple.exe tiny-simple.asm
+REM c:\yasm\yasm.exe -f bin -o tiny.exe tiny.asm
