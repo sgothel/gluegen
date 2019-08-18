5f4292bb8a310489e05a4f3545ae081310a93f72
Sven Gothel <sgothel@jausoft.com>
2019-08-18 23:56:42 +0200

    Build Update: CMake 2.8.10.2 -> 3.15.2 (Windows + MacOS), Mingw64 4.8.1 -> 8.1.0

--- make/scripts/java-win64.bat
+++ make/scripts/java-win64.bat
@@ -9,8 +9,8 @@ set BLD_DIR=..\%BLD_SUB%
 REM set LIB_DIR=..\%BLD_SUB%\obj;..\%BLD_SUB%\test\build\natives
 REM set LIB_DIR=..\%BLD_SUB%\test\build\natives
 
-REM set PATH=%JAVA_HOME%\bin;%ANT_PATH%\bin;c:\mingw\bin;%LIB_DIR%;%PATH%
-set PATH=%JAVA_HOME%\bin;%ANT_PATH%\bin;c:\mingw\bin;%PATH%
+REM set PATH=%JAVA_HOME%\bin;%ANT_PATH%\bin;c:\mingw64\bin;%LIB_DIR%;%PATH%
+set PATH=%JAVA_HOME%\bin;%ANT_PATH%\bin;c:\mingw64\bin;%PATH%
 
 set CP_ALL=.;lib\junit.jar;%ANT_PATH%\lib\ant.jar;%ANT_PATH%\lib\ant-junit.jar;lib/semantic-versioning/semver.jar;lib\TestJarsInJar.jar;%BLD_DIR%\gluegen-rt.jar;%BLD_DIR%\gluegen.jar;%BLD_DIR%\gluegen-test-util.jar;%BLD_DIR%\test\build\gluegen-test.jar
 
--- make/scripts/make.gluegen.all.win32.bat
+++ make/scripts/make.gluegen.all.win32.bat
@@ -6,10 +6,10 @@ set ANT_PATH=C:\apache-ant-1.10.5
 set GIT_PATH=C:\cygwin\bin
 set SEVENZIP=C:\Program Files\7-Zip
 
-set CMAKE_PATH=C:\cmake-2.8.10.2-win32-x86
-set CMAKE_C_COMPILER=c:\mingw\bin\gcc
+set CMAKE_PATH=C:\cmake-3.15.2-win32-x86
+set CMAKE_C_COMPILER=c:\mingw32\bin\gcc
 
-set PATH=%JAVA_HOME%\bin;%ANT_PATH%\bin;c:\mingw\bin;%CMAKE_PATH%\bin;%GIT_PATH%;%SEVENZIP%;%PATH%
+set PATH=%JAVA_HOME%\bin;%ANT_PATH%\bin;c:\mingw32\bin;%CMAKE_PATH%\bin;%GIT_PATH%;%SEVENZIP%;%PATH%
 
 set LIB_GEN=%THISDIR%\lib
 set CLASSPATH=.;%THISDIR%\build-win32\classes
