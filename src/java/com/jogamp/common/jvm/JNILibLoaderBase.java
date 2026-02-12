sgothel/gluegen

commit 2cfa4e877b7f36387ba0a7f6bb7180f72bc6f044

JNILibLoaderBase.loadLibraryInternal: Refine commit b23a84dccb3c6ac4ce6c10687a2bd01e1dbccb67
- Revert to Iterator loop w/ '0 == mode' condition, drop 'break'
- Comments
  - possiblePath maybe empty
  - exception case: own exception only if none could be loaded
  - `0 == mode` final exception in block mode 4, none loaded

src/java/com/jogamp/common/jvm/JNILibLoaderBase.java
@@ -620,22 +620,23 @@ public class JNILibLoaderBase {
                       System.err.println("ERROR mode 3 - "+ex1.getMessage());
                   }
                   final List<NativeLibrary.LibPath> possiblePaths = NativeLibrary.enumerateLibraryPaths(libraryName, libraryName, libraryName, cl);
-                  // Iterate down these and see which one if any we can actually find.
-                  for (NativeLibrary.LibPath path : possiblePaths) {
+                  // Iterate down these and see which one if any we can actually find. possiblePaths maybe empty (-> throw)
+                  for (final Iterator<NativeLibrary.LibPath> iter = possiblePaths.iterator(); 0 == mode && iter.hasNext(); ) {
+                      final NativeLibrary.LibPath path = iter.next();
                       if (DEBUG) {
                           System.err.println("JNILibLoaderBase: System.load("+path+") - mode 4");
                       }
                       try {
                           System.load(path.path);
                           mode = 4;
-                          break;
                       } catch (final UnsatisfiedLinkError ex2) {
                           if(DEBUG) {
                               System.err.println("n/a - "+ex2.getMessage());
                           }
+                          // throw own exception only if none could be loaded
                       }
                   }
-                  if (mode == 0) { // Not loaded, avoid misleading final exception, use our own
+                  if (0 == mode) { // None loaded, throw
                       throw new UnsatisfiedLinkError("Couldn't load library '"+libraryName+
                               "' generically including "+NativeLibrary.getSystemEnvLibraryPaths()+ // mode 3
                               ", nor as "+possiblePaths); // mode 4

commit f50636decbaac1368bbc7d2d50dfa66a56876eb7

Add missing Iterator import
I broke the build in commit 2cfa4e877b7f36387ba0a7f6bb7180f72bc6f044

src/java/com/jogamp/common/jvm/JNILibLoaderBase.java
@@ -47,6 +47,7 @@ import java.net.URL;
 import java.security.PrivilegedAction;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 